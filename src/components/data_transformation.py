8e51f0a71b61de6fc1c40a23fe69388f409f771c
Sanju Sarkar <sarkar.sanju12092001@gmail.com>
2024-05-14 00:12:03 +0530

    Added Data Transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,145 @@
+import sys
+import os
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+def cat_num_features(df):
+
+    '''
+        Utility Function to get the names of Categorical Features and
+        Numerical Features of the given Dataset.
+    '''
+
+    catf = []
+    numf = []
+
+    logging.info("Extracting Categorical and Numerical Features")
+
+    catf = ['type','nameOrig'
+            ,'nameDest','type2']
+
+
+    catf = [feature for feature in catf if feature in df.columns.values]
+    numf = [feature for feature in df.columns if feature not in catf and not feature == 'isFraud']
+
+    return (catf, numf)
+
+
+def type2_create(df):
+    new_type = {'PAYMENT':'OTHERS', 'TRANSFER':'TRANSFER', 'CASH_OUT':'CASH_OUT', 'DEBIT':'OTHERS', 'CASH_IN':'OTHERS'}
+    df['type']=df['type'].map(new_type)
+    df['type'].unique()
+    df['type2'] = np.nan
+    df.loc[df.nameOrig.str.contains('C') & df.nameDest.str.contains('C'), 'type2'] = 'CC'
+    df.loc[df.nameOrig.str.contains('C') & df.nameDest.str.contains('M'), 'type2'] = 'CM'
+    df.loc[df.nameOrig.str.contains('M') & df.nameDest.str.contains('C'), 'type2'] = 'MC'
+    df.loc[df.nameOrig.str.contains('M') & df.nameDest.str.contains('C'), 'type2'] = 'MM'
+    df.drop(columns = ['nameOrig','nameDest','step'], axis = 'columns', inplace = True)
+    return df
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self,df):
+        '''
+        This function si responsible for data trnasformation
+        
+        '''
+        try:
+            categorical_columns, numerical_columns = cat_num_features(df)
+
+            num_pipeline= Pipeline(
+                steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("OneHotEncoding",OneHotEncoder())
+                
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+
+
+            )
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            logging.info("Read train and test data completed")
+            logging.info("New feature is adding")
+            train_df = type2_create(train_df)
+            test_df = type2_create(test_df)
+            logging.info(f"Features are: {train_df.columns}")
+
+            target_column_name="isFraud"
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj=self.get_data_transformer_object(train_df)
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            logging.info(f"Saved preprocessing object.")
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
